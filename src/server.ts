797a71666c4a3a457ebbefb44f3b4779e284b16b
Millena Santos da Costa <millenacosta@id.uff.br>
2025-02-16 18:39:23 -0300

    Add database config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import * as app from "express";
+import app from "express";
 import { UserController } from "./controllers/UserController";
+import init from "./database";
 
 const server = app();
 const router = app.Router();
@@ -18,4 +19,12 @@ router.put("/users/:id", async (req, res) => {
 
 server.use(router);
 
-export default server.listen(3003);
+init()
+	.then(() => {
+		server.listen(3003, () => {
+			console.log("Running port 3003");
+		});
+	})
+	.catch((error) => {
+		console.error("Error connecting to mongodb", error);
+	});
