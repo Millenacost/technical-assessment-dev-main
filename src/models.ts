41107cd305091cb216ed07012825db59273a0db3
Millena Santos da Costa <millenacosta@id.uff.br>
2025-02-17 09:55:37 -0300

    feat: add pre save rules to user model to allow only coordinates or address

--- src/models.ts
+++ src/models.ts
@@ -1,72 +1,89 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 
-import * as mongoose from 'mongoose';
-import { TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
-import { pre, getModelForClass, Prop, Ref, modelOptions } from '@typegoose/typegoose';
-import lib from './lib';
+import * as mongoose from "mongoose";
+import { TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import {
+	pre,
+	getModelForClass,
+	Prop,
+	Ref,
+	modelOptions,
+} from "@typegoose/typegoose";
+import lib from "./lib";
 
 import ObjectId = mongoose.Types.ObjectId;
 
 class Base extends TimeStamps {
-  @Prop({ required: true, default: () => (new ObjectId()).toString() })
-  _id: string;
+	@Prop({ required: true, default: () => new ObjectId().toString() })
+	_id: string;
 }
 
-@pre<User>('save', async function (next) {
-  const region = this as Omit<unknown, keyof User> & User & mongoose.Document;
+@pre<User>("save", async function (next) {
+	console.log("seila");
+	const user = this as Omit<unknown, keyof User> & User & mongoose.Document;
 
-  if (region.isModified('coordinates')) {
-    region.address = await lib.getAddressFromCoordinates(region.coordinates);
-  } else if (region.isModified('address')) {
-    const { lat, lng } = await lib.getCoordinatesFromAddress(region.address);
+	if (user.isModified("coordinates")) {
+		user.address = await lib.getAddressFromCoordinates(user.coordinates);
+	} else if (user.isModified("address")) {
+		const { lat, lng } = await lib.getCoordinatesFromAddress(user.address);
 
-    region.coordinates = [lng, lat];
-  }
+		user.coordinates = [lng, lat];
+	}
 
-  next();
+	if (user.isNew) {
+		if (
+			(user.coordinates && user.address) ||
+			(!user.coordinates && !user.address)
+		) {
+			console.error("You must provide either coordinates or address");
+		}
+	}
+
+	next();
 })
 export class User extends Base {
-  @Prop({ required: true })
-  name!: string;
+	declare _id: string;
 
-  @Prop({ required: true })
-  email!: string;
+	@Prop({ required: true })
+	name!: string;
 
-  @Prop({ required: true })
-  address: string;
+	@Prop({ required: true })
+	email!: string;
 
-  @Prop({ required: true, type: () => [Number] })
-  coordinates: [number, number];
+	@Prop({ required: true })
+	address: string;
 
-  @Prop({ required: true, default: [], ref: () => Region, type: () => String })
-  regions: Ref<Region>[];
+	@Prop({ required: true, type: () => [Number] })
+	coordinates: [number, number];
+
+	@Prop({ required: true, default: [], ref: () => Region, type: () => String })
+	regions: Ref<Region>[];
 }
 
-@pre<Region>('save', async function (next) {
-  const region = this as mongoose.Document & Region;
+@pre<Region>("save", async function (next) {
+	const region = this as mongoose.Document & Region;
 
-  if (!region._id) {
-    region._id = new ObjectId().toString();
-  }
+	if (!region._id) {
+		region._id = new ObjectId().toString();
+	}
 
-  if (region.isNew) {
-    const user = await UserModel.findOne({ _id: region.user });
-    user.regions.push(region._id);
-    await user.save({ session: region.$session() });
-  }
+	if (region.isNew) {
+		const user = await UserModel.findOne({ _id: region.user });
+		user.regions.push(region._id);
+		await user.save({ session: region.$session() });
+	}
 
-  next(region.validateSync());
+	next(region.validateSync());
 })
 @modelOptions({ schemaOptions: { validateBeforeSave: false } })
 export class Region extends Base {
-  @Prop({ required: true, auto: true })
-  _id: string;
+	declare _id: string;
 
-  @Prop({ required: true })
-  name!: string;
+	@Prop({ required: true })
+	name!: string;
 
-  @Prop({ ref: () => User, required: true, type: () => String })
-  user: Ref<User>;
+	@Prop({ ref: () => User, required: true, type: () => String })
+	user: Ref<User>;
 }
 
 export const UserModel = getModelForClass(User);
